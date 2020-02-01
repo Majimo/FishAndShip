4e34a1bd4ccc03733b501789d75524ce2e8d660a
Eizefir <florian.decouflet@aim.com>
2020-02-01 01:00:23 +0100

    Création de la zone pêche

---
+++ scripts/ZonePeche.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D 
+
+var piecesVaisseau = ['piece1','piece2','piece3','piece4','piece5']
+
+func _ready():
+#	print(piecesVaisseau[rand_range(0,piecesVaisseau.size())])
+	pass	
+func pecher():
+	return piecesVaisseau[rand_range(0,piecesVaisseau.size())]
