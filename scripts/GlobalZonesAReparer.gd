c2bec413ba0dc66a511fe9087d165484bb54187b
majimo <pierre.fervel@gmail.com>
2020-02-01 18:08:59 +0100

    ajout de plusieurs zones à réparer

---
+++ scripts/GlobalZonesAReparer.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+#var rng = RandomNumberGenerator.new()
+#
+#func _set_a_module_on_fire():
+#	rng.randomize()
+#	var moduleToBeRepaired = rng.randi_range(0, 5);
+#	get_children()[moduleToBeRepaired / 2].set_on_fire()
